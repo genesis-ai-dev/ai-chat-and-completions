8f20317571ed05b96c619f3aff1810b7eb00fda1
Ben Scholtens <ben.scholtens@me.com>
2024-01-17 15:45:00 -0500

    feat: fix endpoint error

--- utils/customWebviewProvider.js
+++ utils/customWebviewProvider.js
@@ -1,87 +1,100 @@
-const vscode = require('vscode');
-const fs = require('fs');
-const path = require('path');
+const vscode = require("vscode");
+const fs = require("fs");
+const path = require("path");
 
-let config = vscode.workspace.getConfiguration('translators-copilot');
-let endpoint = config.get('endpoint');
-let apiKey = config.get('api_key')
-let model = config.get('model')
-let maxTokens = config.get('max_tokens');
-let temperature = config.get('temperature');
+let config = vscode.workspace.getConfiguration("translators-copilot");
+let endpoint = config.get("llmEndpoint"); // NOTE: config.endpoint is reserved so we must have unique name
+let apiKey = config.get("api_key");
+let model = config.get("model");
+let maxTokens = config.get("max_tokens");
+let temperature = config.get("temperature");
 let maxLength = 4000;
 let abortController = null;
 
 const loadWebviewHtml = (webviewView, extensionUri) => {
   webviewView.webview.options = {
     enableScripts: true,
-    localResourceRoots: [vscode.Uri.file(path.join(extensionUri.fsPath, 'dist'))]
+    localResourceRoots: [
+      vscode.Uri.file(path.join(extensionUri.fsPath, "dist")),
+    ],
   };
 
-  const indexPath = path.join(extensionUri.fsPath, 'dist', 'index.html');
-  let htmlContent = fs.readFileSync(indexPath, 'utf-8');
+  const indexPath = path.join(extensionUri.fsPath, "dist", "index.html");
+  let htmlContent = fs.readFileSync(indexPath, "utf-8");
 
   const resourceRegEx = /<(script|link).*?(src|href)="([^"]*?)".*?\/?>/g;
-  htmlContent = htmlContent.replace(resourceRegEx, (match, tag, attribute, src) => {
-    const resourcePath = vscode.Uri.file(path.join(extensionUri.fsPath, 'dist', src));
-    console.log({ resourcePath })
-    const resourceUri = webviewView.webview.asWebviewUri(resourcePath);
-    return `<${tag} ${attribute}="${resourceUri}" ${tag === 'link' ? 'rel="stylesheet"' : ''}></${tag}>`;
-  });
+  htmlContent = htmlContent.replace(
+    resourceRegEx,
+    (match, tag, attribute, src) => {
+      const resourcePath = vscode.Uri.file(
+        path.join(extensionUri.fsPath, "dist", src)
+      );
+      console.log({ resourcePath });
+      const resourceUri = webviewView.webview.asWebviewUri(resourcePath);
+      return `<${tag} ${attribute}="${resourceUri}" ${
+        tag === "link" ? 'rel="stylesheet"' : ""
+      }></${tag}>`;
+    }
+  );
 
   webviewView.webview.html = htmlContent;
-}
+};
 
 const sendFinishMessage = (webviewView) => {
   webviewView.webview.postMessage({
-    command: 'response',
+    command: "response",
     finished: true,
-    text: ''
+    text: "",
   });
-}
+};
 
 const processFetchResponse = (webviewView, response) => {
   return new Promise((resolve, reject) => {
     try {
-      const reader = response.body.getReader()
-      let decoder = new TextDecoder('utf-8')
+      const reader = response.body.getReader();
+      let decoder = new TextDecoder("utf-8");
 
-      reader.read().then(
-        function processText({ done, value }) {
-        if (done) {
-          sendFinishMessage(webviewView)
-          resolve()
-          return
-        }
-        let chunk = decoder.decode(value)
-        // Split using 'data:'
-        let chunkArray = chunk.split("data:");
-        chunkArray.forEach((jsonString) => {
-          jsonString = jsonString.trim()
-          // Check if the split string is empty
-          if (jsonString.length > 0) {
-            try {
-              const payload = JSON.parse(jsonString)
-              console.log('29u3089u', { payload })
-              let payloadTemp = payload['choices'][0]
-              let sendChunk = payloadTemp['message'] ? payloadTemp['message']['content'] : payloadTemp['delta']['content']
-              sendChunk && webviewView.webview.postMessage({
-                command: 'response',
-                finished: false,
-                text: sendChunk
-              })
-            } catch (error) {
-              console.log('Error:', error)
-            }
+      reader
+        .read()
+        .then(function processText({ done, value }) {
+          if (done) {
+            sendFinishMessage(webviewView);
+            resolve();
+            return;
           }
+          let chunk = decoder.decode(value);
+          // Split using 'data:'
+          let chunkArray = chunk.split("data:");
+          chunkArray.forEach((jsonString) => {
+            jsonString = jsonString.trim();
+            // Check if the split string is empty
+            if (jsonString.length > 0) {
+              try {
+                const payload = JSON.parse(jsonString);
+                console.log("29u3089u", { payload });
+                let payloadTemp = payload["choices"][0];
+                let sendChunk = payloadTemp["message"]
+                  ? payloadTemp["message"]["content"]
+                  : payloadTemp["delta"]["content"];
+                sendChunk &&
+                  webviewView.webview.postMessage({
+                    command: "response",
+                    finished: false,
+                    text: sendChunk,
+                  });
+              } catch (error) {
+                console.log("Error:", error);
+              }
+            }
+          });
+          return reader.read().then(processText);
         })
-        return reader.read().then(processText)
-      }).catch(reject)
+        .catch(reject);
     } catch (error) {
-      reject(error)
+      reject(error);
     }
-  })
-}
-
+  });
+};
 
 class CustomWebviewProvider {
   constructor(extensionUri) {
@@ -90,7 +103,7 @@ class CustomWebviewProvider {
 
   sendSelectMessage(webviewView, selectedText) {
     const activeEditor = vscode.window.activeTextEditor;
-    let languageId = '';
+    let languageId = "";
     if (activeEditor) {
       languageId = activeEditor.document.languageId;
     }
@@ -98,32 +111,34 @@ class CustomWebviewProvider {
     if (selectedText.length > maxLength - 100) {
       selectedText = selectedText.substring(0, maxLength - 100);
     }
-    const formattedCode = '```' + languageId + '\r\n' + selectedText + '\r\n```';
+    const formattedCode =
+      "```" + languageId + "\r\n" + selectedText + "\r\n```";
     webviewView.webview.postMessage({
-      command: 'select',
-      text: selectedText ? formattedCode : ''
-    })
+      command: "select",
+      text: selectedText ? formattedCode : "",
+    });
   }
 
   saveSelectionChanges(webviewView) {
     const activeEditor = vscode.window.activeTextEditor;
     if (activeEditor) {
-      this.selectionChangeListener = vscode.window.onDidChangeTextEditorSelection(e => {
-        if (e.textEditor === activeEditor) {
-          const selectedText = activeEditor.document.getText(e.selections[0]);
-          this.sendSelectMessage(webviewView, selectedText);
-        }
-      });
+      this.selectionChangeListener =
+        vscode.window.onDidChangeTextEditorSelection((e) => {
+          if (e.textEditor === activeEditor) {
+            const selectedText = activeEditor.document.getText(e.selections[0]);
+            this.sendSelectMessage(webviewView, selectedText);
+          }
+        });
     }
   }
 
   resolveWebviewView(webviewView) {
     loadWebviewHtml(webviewView, this._extensionUri);
-    webviewView.webview.postMessage({ command: 'reload' });
+    webviewView.webview.postMessage({ command: "reload" });
 
     webviewView.onDidChangeVisibility(() => {
       if (webviewView.visible) {
-        webviewView.webview.postMessage({ command: 'reload' });
+        webviewView.webview.postMessage({ command: "reload" });
       }
     });
 
@@ -139,48 +154,49 @@ class CustomWebviewProvider {
     webviewView.webview.onDidReceiveMessage(async (message) => {
       try {
         switch (message.command) {
-          case 'fetch':
+          case "fetch":
             abortController = new AbortController();
             const url = endpoint + "/chat/completions";
+            console.log({ url, endpoint, apiKey });
             const data = {
               max_tokens: maxTokens,
               temperature: temperature,
               stream: true,
-              messages: JSON.parse(message.messages)
+              messages: JSON.parse(message.messages),
             };
             if (model) {
-              data.model = model
+              data.model = model;
             }
             const headers = {
-              "Content-Type": "application/json"
+              "Content-Type": "application/json",
             };
             if (apiKey) {
-              headers["Authorization"] = "Bearer " + apiKey
+              headers["Authorization"] = "Bearer " + apiKey;
             }
             const response = await fetch(url, {
-              method: 'POST',
+              method: "POST",
               headers,
               body: JSON.stringify(data),
-              signal: abortController.signal
-            })
-            await processFetchResponse(webviewView, response)
+              signal: abortController.signal,
+            });
+            await processFetchResponse(webviewView, response);
             break;
-          case 'abort-fetch':
+          case "abort-fetch":
             if (abortController) {
-              abortController.abort()
+              abortController.abort();
             }
-            break
+            break;
           default:
-            break
+            break;
         }
       } catch (error) {
         sendFinishMessage(webviewView);
         console.error("Error:", error);
-        vscode.window.showErrorMessage("Service access failed.")
+        vscode.window.showErrorMessage("Service access failed.");
       }
     });
   }
 }
 module.exports = {
-  CustomWebviewProvider
+  CustomWebviewProvider,
 };
--- utils/inlineCompletionProvider.js
+++ utils/inlineCompletionProvider.js
@@ -1,25 +1,35 @@
-const vscode = require('vscode');
-const axios = require('axios');
+const vscode = require("vscode");
+const axios = require("axios");
 
-let config = vscode.workspace.getConfiguration('translators-copilot');
-let endpoint = config.get('endpoint');
-let apiKey = config.get('api_key')
-let model = config.get('model')
-let temperature = config.get('temperature');
-let maxTokens = config.get('max_tokens');
+let config = vscode.workspace.getConfiguration("translators-copilot");
+let endpoint = config.get("llmEndpoint"); // NOTE: config.endpoint is reserved so we must have unique name
+let apiKey = config.get("api_key");
+let model = config.get("model");
+let temperature = config.get("temperature");
+let maxTokens = config.get("max_tokens");
 let maxLength = 4000;
 let shouldProvideCompletion = false;
-
-async function provideInlineCompletionItems(document, position, context, token) {
+console.log({ config });
+async function provideInlineCompletionItems(
+  document,
+  position,
+  context,
+  token
+) {
   vscode.window.showInformationMessage("provideInlineCompletionItems called");
   if (!shouldProvideCompletion) {
     return undefined;
   }
-  const text = (model.startsWith('gpt') && (
-    endpoint.startsWith("https://api") ||
-    endpoint.startsWith("https://localhost")
-  )) ? await getCompletionTextGPT(document, position) : await getCompletionText(document, position);
-  let completionItem = new vscode.InlineCompletionItem(text, new vscode.Range(position, position));
+  const text =
+    model.startsWith("gpt") &&
+    (endpoint.startsWith("https://api") ||
+      endpoint.startsWith("https://localhost"))
+      ? await getCompletionTextGPT(document, position)
+      : await getCompletionText(document, position);
+  let completionItem = new vscode.InlineCompletionItem(
+    text,
+    new vscode.Range(position, position)
+  );
   completionItem.range = new vscode.Range(position, position);
   shouldProvideCompletion = false;
   return [completionItem];
@@ -31,7 +41,7 @@ function preprocessDocument(docText) {
   let lines = docText.split("\r\n");
   // Apply preprocessing rules to each line
   for (let i = 0; i < lines.length; i++) {
-    if (i > 0 && lines[i - 1].trim() !== '' && isStartWithComment(lines[i])) {
+    if (i > 0 && lines[i - 1].trim() !== "" && isStartWithComment(lines[i])) {
       lines[i] = "\r\n" + lines[i];
     }
   }
@@ -42,18 +52,22 @@ function preprocessDocument(docText) {
 function isStartWithComment(line) {
   let trimLine = line.trim();
   // Define a list of comment start symbols
-  let commentStartSymbols = ['//', '#', '/*', '<!--', '{/*'];
+  let commentStartSymbols = ["//", "#", "/*", "<!--", "{/*"];
   for (let symbol of commentStartSymbols) {
-    if (trimLine.startsWith(symbol))
-      return true;
+    if (trimLine.startsWith(symbol)) return true;
   }
   return false;
 }
 
 async function getCompletionText(document, position) {
   let language = document.languageId;
-  let textBeforeCursor = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
-  textBeforeCursor = textBeforeCursor.length > maxLength ? textBeforeCursor.substr(textBeforeCursor.length - maxLength) : textBeforeCursor;
+  let textBeforeCursor = document.getText(
+    new vscode.Range(new vscode.Position(0, 0), position)
+  );
+  textBeforeCursor =
+    textBeforeCursor.length > maxLength
+      ? textBeforeCursor.substr(textBeforeCursor.length - maxLength)
+      : textBeforeCursor;
 
   // Preprocess the document before the cursor
   textBeforeCursor = preprocessDocument(textBeforeCursor);
@@ -63,8 +77,8 @@ async function getCompletionText(document, position) {
 
   let lineContent = document.lineAt(position.line).text;
   let leftOfCursor = lineContent.substr(0, position.character).trim();
-  if (leftOfCursor !== '') {
-    stop.push('\r\n');
+  if (leftOfCursor !== "") {
+    stop.push("\r\n");
   }
 
   if (textBeforeCursor) {
@@ -74,78 +88,107 @@ async function getCompletionText(document, position) {
   }
 
   let data = {
-    "prompt": prompt,
-    "max_tokens": 256,
-    "temperature": temperature,
-    "stream": false,
-    "stop": stop,
-    "n": 2,
+    prompt: prompt,
+    max_tokens: 256,
+    temperature: temperature,
+    stream: false,
+    stop: stop,
+    n: 2,
   };
   if (model) {
     data.model = model;
   }
   const headers = {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   };
   if (apiKey) {
-    headers['Authorization'] = 'Bearer ' + apiKey;
+    headers["Authorization"] = "Bearer " + apiKey;
   }
   let config = {
-    method: 'POST',
-    url: endpoint + '/completions',
+    method: "POST",
+    url: endpoint + "/completions",
     headers,
-    data: JSON.stringify(data)
+    data: JSON.stringify(data),
   };
 
   try {
     const response = await axios.request(config);
-    if (response && response.data && response.data.choices && response.data.choices.length > 0) {
-      return response.data.choices[0].text.replace(/[\r\n]+$/g, '');
+    if (
+      response &&
+      response.data &&
+      response.data.choices &&
+      response.data.choices.length > 0
+    ) {
+      return response.data.choices[0].text.replace(/[\r\n]+$/g, "");
     }
   } catch (error) {
     console.log("Error:", error.message);
-    vscode.window.showErrorMessage("Service access failed.")
+    vscode.window.showErrorMessage("Service access failed.");
   }
 }
 
 async function getCompletionTextGPT(document, position) {
   vscode.window.showInformationMessage("getCompletionTextGPT called");
-  let textBeforeCursor = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
-  textBeforeCursor = textBeforeCursor.length > maxLength ? textBeforeCursor.substr(textBeforeCursor.length - maxLength) : textBeforeCursor;
+  let textBeforeCursor = document.getText(
+    new vscode.Range(new vscode.Position(0, 0), position)
+  );
+  textBeforeCursor =
+    textBeforeCursor.length > maxLength
+      ? textBeforeCursor.substr(textBeforeCursor.length - maxLength)
+      : textBeforeCursor;
   textBeforeCursor = preprocessDocument(textBeforeCursor);
   const url = endpoint + "/chat/completions";
+  console.log({ url });
   const messages = [
-    { "role": "system", "content": "No communication! Just continue writing the code provided by the user." },
-    { "role": "user", "content": textBeforeCursor }
-  ]
+    {
+      role: "system",
+      content:
+        "No communication! Just continue writing the code provided by the user.",
+    },
+    { role: "user", content: textBeforeCursor },
+  ];
   const data = {
     max_tokens: maxTokens,
     temperature,
     model,
     stream: false,
     messages,
-    stop: ["\n\n", "\r\r", "\r\n\r", "\n\r\n"]
+    stop: ["\n\n", "\r\r", "\r\n\r", "\n\r\n"],
   };
   const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + apiKey
-  }
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + apiKey,
+  };
   let text = "";
   try {
     const config = {
-      method: 'POST',
+      method: "POST",
       url,
       headers,
-      data: JSON.stringify(data)
-    }
+      data: JSON.stringify(data),
+    };
     const response = await axios.request(config);
-    if (response && response.data && response.data.choices && response.data.choices.length > 0) {
+    if (
+      response &&
+      response.data &&
+      response.data.choices &&
+      response.data.choices.length > 0
+    ) {
       text = response.data.choices[0].message.content;
       if (text.startsWith("```")) {
-        const textLines = text.split('\n');
-        const startIndex = textLines.findIndex(line => line.startsWith("```"));
-        const endIndex = textLines.slice(startIndex + 1).findIndex(line => line.startsWith("```"));
-        text = endIndex >= 0 ? textLines.slice(startIndex + 1, startIndex + endIndex + 1).join('\n') : textLines.slice(startIndex + 1).join('\n');
+        const textLines = text.split("\n");
+        const startIndex = textLines.findIndex((line) =>
+          line.startsWith("```")
+        );
+        const endIndex = textLines
+          .slice(startIndex + 1)
+          .findIndex((line) => line.startsWith("```"));
+        text =
+          endIndex >= 0
+            ? textLines
+                .slice(startIndex + 1, startIndex + endIndex + 1)
+                .join("\n")
+            : textLines.slice(startIndex + 1).join("\n");
       }
     }
   } catch (error) {
@@ -157,10 +200,10 @@ async function getCompletionTextGPT(document, position) {
 
 function triggerInlineCompletion() {
   shouldProvideCompletion = true;
-  vscode.commands.executeCommand('editor.action.inlineSuggest.trigger');
+  vscode.commands.executeCommand("editor.action.inlineSuggest.trigger");
 }
 
 module.exports = {
   provideInlineCompletionItems,
-  triggerInlineCompletion
-}
+  triggerInlineCompletion,
+};
